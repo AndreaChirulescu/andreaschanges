b9367d910647de09365d8e0c06ac4d72447ddc30
Harald Eilertsen <haraldei@anduin.net>
2021-01-16 13:33:45 +0100

    Initial commit, set the stage.

---
+++ gigologadmin.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Gigolog Admin
+ *
+ * @package     gigologadmin
+ * @author      Andrea Chirulescu, Harald Eilertsen
+ * @copright    2021 Andrea Chirulescu, Harald Eilertsen
+ * @license     AGPL-3.0
+ *
+ * @wordpress-plugin
+ * Plugin Name: Gigolog Admin
+ * Plugin URI:  https://code.volse.no/wp/plugins/gigologadmin
+ * Description: Scheduling journalists and photographers to cover concerts or events.
+ * Version:     0.1.0
+ * Author:      Andrea Chirulescu, Harald Eilertsen
+ * License:     AGPLv3
+ * License URI: https://www.gnu.org/licenses/agpl-3.0.txthttps://www.gnu.org/licenses/agpl-3.0.txt
+ */
+?>
