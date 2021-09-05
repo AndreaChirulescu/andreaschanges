30e7ddc5766aed311c7ea5571e63ae54386b3af1
Harald Eilertsen <haraldei@anduin.net>
2021-09-05 15:02:49 +0200

    Fix tests.
    Since the plugin only should load on the admin side, set the WP_ADMIN
    constant before loading it in the tests.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -30,6 +30,7 @@ require_once $_tests_dir . '/includes/functions.php';
  * Manually load the plugin being tested.
  */
 function _manually_load_plugin() {
+    define('WP_ADMIN', true);     // We're always in admin
 	require dirname( dirname( __FILE__ ) ) . '/giglogadmin.php';
 }
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
