87d6572d92b10ae9f987efbb84360542eb3d15dc
Harald Eilertsen <haraldei@anduin.net>
2022-02-12 19:50:47 +0100

    Fix indentation and add license info.

--- includes/giglog_visitor_display.php
+++ includes/giglog_visitor_display.php
@@ -1,4 +1,9 @@
 <?php declare(strict_types=1);
+// SPDX-FileCopyrightText: 2022 Andrea Chirulescu <andrea.chirulescu@gmail.com>
+// SPDX-FileCopyrightText: 2022 Harald Eilertsen <haraldei@anduin.net>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 function display_giglog() : string
 {
     $c = new GiglogAdmin_ConcertsTable();
