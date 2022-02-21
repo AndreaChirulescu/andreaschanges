ef1da20cb88b94d7c4781cb3bcf86ccb67c5005f
Harald Eilertsen <haraldei@anduin.net>
2022-02-21 21:04:38 +0100

    Add sample ical export module.
    Currently just uses sample from iCalcreator docs, but shows how it can
    be done, and how to hook it into WordPress.

--- giglogadmin.php
+++ giglogadmin.php
@@ -31,9 +31,11 @@ if ( !class_exists( 'GiglogAdmin_Plugin' ) ) {
                 require_once __DIR__ . '/includes/admin/register_db_tables.php';
             }
 
+            require_once __DIR__ . '/vendor/autoload.php';
             require_once __DIR__ . '/includes/venue.php';
             require_once __DIR__ . '/includes/concert.php';
             require_once __DIR__ . '/includes/view-helpers/select_field.php';
+            require_once __DIR__ . '/includes/ical_export.php';
 
             if (is_admin()) {
                 require_once __DIR__ . '/includes/admin/views/giglog_admin_page.php';
